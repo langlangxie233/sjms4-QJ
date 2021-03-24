1aa6145b7bfc2e1e00f7009464d7f520a74e9f95
langlangxie233 <80050656+langlangxie233@users.noreply.github.com>
2021-03-24 08:39:29 +0000

    Create UnixImp.java

---
+++ src/com/system/UnixImp.java
@@ -0,0 +1,10 @@
+package com.system;
+
+import com.Matrix;
+
+public class UnixImp extends ImageImp {
+    @Override
+    public void doPaint(Matrix m) {
+        System.out.println("在Unix系统显示转化为图像矩阵m的图片！");
+    }
+}
